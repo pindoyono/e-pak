1eea8ff80ca168be65c1e4b8a7bbb02015c194c3
Patan Pindoyono <pindoyonopatan@gmail.com>
2020-09-04 14:38:47 +0800

    coba

--- resources/views/auth/login1.blade.php
+++ resources/views/auth/login1.blade.php
@@ -25,6 +25,7 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
+                                    
                             </div>
                         </div>
                         <div class="input-group">
